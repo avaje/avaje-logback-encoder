2348597b508b9f525bd26631e6a1ec003bb7b862
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-09-21 23:15:21 -0400

    add slf4j requires

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,5 +7,8 @@ module io.avaje.logback.encoder {
   requires transitive ch.qos.logback.classic;
   requires transitive ch.qos.logback.core;
   requires transitive io.avaje.jsonb;
+  requires transitive org.slf4j;
+  requires transitive org.slf4j.jdk.platform.logging;
+  requires java.naming;
 
 }
